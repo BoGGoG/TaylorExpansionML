6a0cc52df3fa41e11ba75bed4faef96558d91d82
BoGGoG <marco.knipfer@me.com>
2022-05-23 19:08:17 +0200

    Correct prefix notation
    I think I have managed to get the correct prefix notation for a sympy formula.
    The main problem was that multiplication and addition in sympy do not have a fixed number of arguments,
    but variable one, so mul a b c would be a*b*c, but I want mul a mul b c.
    Took some inspiration from https://github.com/facebookresearch/SymbolicMathematics,
    like the list for the operators, since I could not find it in sympy.

---
+++ dev/sympy_to_prefix.py
@@ -0,0 +1,74 @@
+import os
+import sys
+import inspect
+from icecream import ic
+import sympy
+from sympy.parsing.sympy_parser import repeated_decimals
+
+script_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+root_dir = os.path.dirname(script_dir)
+sys.path.insert(0, root_dir)
+
+
+from source.data_preparation import sympy_tokenize, sympy_tokenize_str, vectorize_ds, vectorize_sentence, pad_right, sympy_to_prefix, repeat_operator_until_correct_binary
+x = sympy.Symbol("x")
+expr0a = sympy.parsing.parse_expr("x")
+expr0b = sympy.parsing.parse_expr("1")
+expr0c = sympy.parsing.parse_expr("2")
+expr0d = sympy.parsing.parse_expr("sin(x)")
+expr0e = sympy.parsing.parse_expr("sin(cos(x))")
+expr0f = sympy.parsing.parse_expr("tan(sin(cos(x)))")
+expr1a = sympy.parsing.parse_expr("x + sin(y+z)")
+expr1b = sympy.parsing.parse_expr("x + sinh(cos(x**2)) + 3")
+expr1c = sympy.parsing.parse_expr("x + sinh(cos(x**2)) + 3 + 1 + exp(3)")
+expr2a = sympy.parsing.parse_expr("abs(y) + cot(x)")
+expr2b = sympy.parsing.parse_expr("1/x + acsc(1+x+asec(y))")
+expr2c = sympy.parsing.parse_expr("sign(x) + ln(1/x) + exp(-x**2)")
+
+# ic(repeat_operator_until_correct_binary("sin", ["x"]))
+# ic(repeat_operator_until_correct_binary("mul", ["x", "y", "z"]))
+# ic(repeat_operator_until_correct_binary("mul", ["a", "x", "y", "z"]))
+# ic(repeat_operator_until_correct_binary("add", ["a", "x", "y", "z"]))
+# ic(repeat_operator_until_correct_binary("add", ["a", "x"]))
+# ic(repeat_operator_until_correct_binary("add", ["a", "x", ["sin", "x"]]))
+
+# ic(expr)
+# ic(type(expr.args))
+# ic(expr.args)
+# ic(sympy_to_prefix(expr))
+
+ic(expr0a)
+ic(expr0b)
+ic(expr0c)
+ic(expr0d)
+ic(expr0e)
+ic(expr0f)
+
+ic(expr1a)
+ic(expr1b)
+ic(expr1c)
+
+ic(expr2a)
+ic(expr2b)
+ic(expr2c)
+
+# ic(expr0b)
+# ic(expr1)
+# ic(expr2)
+ic(sympy_to_prefix(expr0a))
+ic(sympy_to_prefix(expr0b))
+ic(sympy_to_prefix(expr0c))
+ic(sympy_to_prefix(expr0d))
+ic(sympy_to_prefix(expr0e))
+ic(sympy_to_prefix(expr0f))
+
+ic(sympy_to_prefix(expr1a))
+ic(sympy_to_prefix(expr1b))
+ic(sympy_to_prefix(expr1c))
+
+ic(sympy_to_prefix(expr2a))
+ic(sympy_to_prefix(expr2b))
+ic(sympy_to_prefix(expr2c))
+
+
+
--- source/data_preparation.py
+++ source/data_preparation.py
@@ -1,6 +1,125 @@
-import sympy
+from itertools import repeat
+import sympy as sp
 from sympy import *
 import numpy as np
+from icecream import ic
+
+# operators and operators_nargs are from 
+# https://github.com/facebookresearch/SymbolicMathematics
+
+operators = {
+    # Elementary functions
+    sp.Add: 'add',
+    sp.Mul: 'mul',
+    sp.Pow: 'pow',
+    sp.exp: 'exp',
+    sp.log: 'ln',
+    sp.Abs: 'abs',
+    sp.sign: 'sign',
+    # Trigonometric Functions
+    sp.sin: 'sin',
+    sp.cos: 'cos',
+    sp.tan: 'tan',
+    sp.cot: 'cot',
+    sp.sec: 'sec',
+    sp.csc: 'csc',
+    # Trigonometric Inverses
+    sp.asin: 'asin',
+    sp.acos: 'acos',
+    sp.atan: 'atan',
+    sp.acot: 'acot',
+    sp.asec: 'asec',
+    sp.acsc: 'acsc',
+    # Hyperbolic Functions
+    sp.sinh: 'sinh',
+    sp.cosh: 'cosh',
+    sp.tanh: 'tanh',
+    sp.coth: 'coth',
+    sp.sech: 'sech',
+    sp.csch: 'csch',
+    # Hyperbolic Inverses
+    sp.asinh: 'asinh',
+    sp.acosh: 'acosh',
+    sp.atanh: 'atanh',
+    sp.acoth: 'acoth',
+    sp.asech: 'asech',
+    sp.acsch: 'acsch',
+    # Derivative
+    sp.Derivative: 'derivative',
+}
+
+operators_nargs = {
+    # Elementary functions
+    'add': 2,
+    'sub': 2,
+    'mul': 2,
+    'div': 2,
+    'pow': 2,
+    'rac': 2,
+    'inv': 1,
+    'pow2': 1,
+    'pow3': 1,
+    'pow4': 1,
+    'pow5': 1,
+    'sqrt': 1,
+    'exp': 1,
+    'ln': 1,
+    'abs': 1,
+    'sign': 1,
+    # Trigonometric Functions
+    'sin': 1,
+    'cos': 1,
+    'tan': 1,
+    'cot': 1,
+    'sec': 1,
+    'csc': 1,
+    # Trigonometric Inverses
+    'asin': 1,
+    'acos': 1,
+    'atan': 1,
+    'acot': 1,
+    'asec': 1,
+    'acsc': 1,
+    # Hyperbolic Functions
+    'sinh': 1,
+    'cosh': 1,
+    'tanh': 1,
+    'coth': 1,
+    'sech': 1,
+    'csch': 1,
+    # Hyperbolic Inverses
+    'asinh': 1,
+    'acosh': 1,
+    'atanh': 1,
+    'acoth': 1,
+    'asech': 1,
+    'acsch': 1,
+    # Derivative
+    'derivative': 2,
+    # custom functions
+    'f': 1,
+    'g': 2,
+    'h': 3,
+}
+
+def flatten(l, ltypes=(list, tuple)):
+    """
+    flatten a python list
+    from http://rightfootin.blogspot.com/2006/09/more-on-python-flatten.html
+    """
+    ltype = type(l)
+    l = list(l)
+    i = 0
+    while i < len(l):
+        while isinstance(l[i], ltypes):
+            if not l[i]:
+                l.pop(i)
+                i -= 1
+                break
+            else:
+                l[i:i + 1] = l[i]
+        i += 1
+    return ltype(l)
 
 
 def pad_right(list, total_length=4, const=0):
@@ -11,14 +130,14 @@ def pad_right(list, total_length=4, const=0):
     else:
         return list[0:total_length]
 
-def sympy_tokenize(expr, tokens_list=[], depth=0, parent_ind=None):
-    if (expr.func == sympy.core.symbol.Symbol) | (expr.func == sympy.core.numbers.Integer):
+def sympy_tokenize(expr, tokens_list=[], depth=0):
+    if (expr.func == sp.core.symbol.Symbol) | (expr.func == sp.core.numbers.Integer):
         to_append = expr
     else:
         to_append = expr.func
     tokens_list.append(to_append)
-    for ind, arg in enumerate(expr.args):
-        sympy_tokenize(arg, tokens_list, depth+1, parent_ind=ind)
+    for arg in expr.args:
+        sympy_tokenize(arg, tokens_list, depth+1)
     return tokens_list
 
 def sympy_tokenize_str(sentence):
@@ -46,3 +165,73 @@ def vectorize_ds(X_tokenized_str, model, sequence_length=25):
     # sequence_length = np.max([len(Xi) for Xi in X_vectorized]) + 5
     X_vectorized = [pad_right(Xi, sequence_length, const=0) for Xi in X_vectorized]
     return X_vectorized
+
+atoms = [str, sp.core.symbol.Symbol, sp.core.Integer, sp.core.numbers.One, sp.core.numbers.NegativeOne]
+
+def sympy_to_prefix(expression):
+    """
+    Recursively go from a sympy expression to a prefix notation.
+    Returns a flat list of tokens.
+    """
+    return flatten(sympy_to_prefix_rec(expression, []))
+
+def sympy_to_prefix_rec(expression, ret):
+    """
+    Recursively go from a sympy expression to a prefix notation.
+    The operators all get converted to their names in the array `operators`.
+    Returns a nested list, where the nesting basically stands for parentheses.
+    Since in prefix notation with a fixed number of arguments for each function (given in `operators_nargs`),
+    parentheses are not needed, we can flatten the list later.
+    """
+    f = expression.func
+    if f in atoms:
+        return ret+[expression]
+    f_str = operators[f]
+    f_nargs = operators_nargs[f_str]
+    args = expression.args
+    if len(args) == 1 & f_nargs == 1: 
+        ret = ret + [f_str]
+        return sympy_to_prefix_rec(args[0], ret)
+    if len(args) == 2:
+        ret = ret + [f_str, sympy_to_prefix_rec(args[0], []), sympy_to_prefix_rec(args[1], [])]
+    if len(args) > 2:
+        args = list(map(lambda x: sympy_to_prefix_rec(x, []), args))
+        ret = ret + repeat_operator_until_correct_binary(f_str, args)
+    return ret 
+
+def repeat_operator_until_correct_binary(op, args, ret=[]):
+    """
+    sympy is not strict enough with the number of arguments.
+    E.g. multiply takes a variable number of arguments, but for 
+    prefix notation it needs to ALWAYS have exactly 2 arguments
+
+    This function is only for binary operators.
+
+    Here I choose the convention as follows:
+        1 + 2 + 3 --> + 1 + 2 3 
+    
+    This is the same convention as in https://arxiv.org/pdf/1912.01412.pdf
+    on page 15.
+
+    input:
+        op: in string form as in the list `operators`
+        args: [arg1, arg2, ...] arguments of the operator, e.c. [1, 2, x**2,
+                ...]. They can have other things to be evaluated in them
+        ret: the list you already have. Usually []. Watch out, I think one has to explicitely give [],
+            otherwise somehow the default value gets mutated, which I find a strange python behavior.
+    """
+
+    is_binary = operators_nargs[op] == 2
+    assert is_binary, "repeat_operator_until_correct_binary only takes binary operators" 
+
+    if len(args) == 0:
+        return ret
+    elif len(ret) == 0:
+        ret = [op] + args[-2:]
+        args = args[:-2]
+    else:
+        ret = [op] + args[-1:] + ret
+        args = args[:-1]
+
+    return repeat_operator_until_correct_binary(op, args, ret)
+
